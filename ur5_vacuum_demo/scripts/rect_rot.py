4676d3b956b6009f34e3cd6d8da1adf94b0fd08a
subbotinny <neymarus@yandex.ru>
2023-06-09 00:44:05 +0000

    Added: corner calculation

--- ur5_vacuum_demo/scripts/rect_rot.py
+++ ur5_vacuum_demo/scripts/rect_rot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import cv2
 import rospy
+import numpy as np
+from math import sqrt
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -8,31 +10,34 @@ from cv_bridge import CvBridge, CvBridgeError
 def camera():
     rospy.init_node('camera_read', anonymous=False)
     image_sub = rospy.Subscriber("/camera/image_raw", Image, callback)
-    
+
+def corner_calc(points):
+    x1, y1 = points[0]
+    x2, y2 = points[1]
+    x3, y3 = points[2]
+    if(sqrt((x3 - x2)**2 + (y3 - y2)**2) > sqrt((x1 - x2)**2 + (y1 - y2)**2)):
+        x1 = x3
+        y1 = y3
+    return round(np.arctan((x1 - x2)/(y1 - y2)), 3)
+
 def callback(data):
     bridge = CvBridge()
 
     orig_image = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
-    image = orig_image[140:340, 140:340] #crop
+    image = orig_image[165:315, 165:315] #crop
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #grayscale
     blur = cv2.GaussianBlur(gray, (9,9), 0)
-    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 51, 3)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 51, 2)
 
-    contours, hier = cv2.findContours(thresh, 1, 2)
-    #print("Number of detected contours", len(contours))
-    
-    for cnt in contours:
-    	x1, y1 = cnt[0][0]
-    	approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-    	if len(approx) == 4:
-            x, y, w, h = cv2.boundingRect(cnt)
-            ratio = float(w)/h
-            cv2.putText(image, 'SMD'+str(ratio), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
-            image = cv2.drawContours(image, [cnt], -1, (0,255,0), 3)
-
+    contours, hier = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    rect = cv2.minAreaRect(contours[0])
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    if len(box) == 4:
+        print(corner_calc(box))
+    cv2.drawContours(image, [box], 0, (0,255,0), 3)
     cv2.imshow("Camera output", image)
-    cv2.waitKey(1000)
+    cv2.waitKey(100)
     
 camera()
 rospy.spin()
-
